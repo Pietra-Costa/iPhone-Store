4da8e242d25e9c094a6f794b3900fb5461cbf39c
Pietra Costa <pietracosta.dev@gmail.com>
2024-07-21 20:14:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider (e){
+    document.querySelector("#phone").src = e;
+}
+
+function circleSlider (f){
+    const circle = document.querySelector(".circle");
+    circle.style.background = f;
+}
